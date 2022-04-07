092f226cc846a799e80de3f353b4c7335b444bc2
Nancy4Hany <nancyhany8@gmail.com>
2022-04-07 22:46:53 +0200

    new updates added

---
+++ classes/DB.php
@@ -0,0 +1,22 @@
+
+<?php
+  // Send a raw HTTP header
+class DB{
+
+    private static function connect(){
+        $pdo = new PDO('mysql:host=localhost;dbname=nancy;charset=utf8;collation=utf8_unicode_ci', 'root', '');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $pdo;
+    }
+    
+    public static function query($query, $params = array()){
+            $statement = self::connect()->prepare($query);
+            $statement->execute($params);
+
+            if(explode(' ', $query)[0] == 'SELECT'){
+            $data = $statement->fetchAll();
+            return $data;
+            }
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./**/*.php","./**/*.html","./layout/**/*.js"],
+  theme: {
+    screens : {
+      sm : '480 px' , 
+      md : '768 px' ,
+      lg : '976 px',
+      xl : '1440 px'
+
+    },
+    extend: {}
+  },
+  plugins: [],
+}
