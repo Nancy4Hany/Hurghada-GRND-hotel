dca21be55e7a58d90f385c1ff1d3fd8e4273567a
sarah801 <sarah2017578@miuegypt.edu.eg>
2022-05-11 15:37:28 +0200

    front-end basics: login :

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,22 @@ module.exports = {
       xl : '1440 px'
 
     },
-    extend: {}
+    extend: {
+      fontFamily :
+      {sans : ['Poppins','sans-serif' ,'lato']
+    },
+    colors :  {
+      grey : {
+        50: '#F9FAFB', 
+        300 : '#F1F1F1', 
+        400: '#E0E0E0' , 
+        500 : '#AEAEAE' , 
+        600 : '#7E7E7E' , 
+        900 : '#303030' , 
+      }
+   
+    }
+    }
   },
   plugins: [],
 }
