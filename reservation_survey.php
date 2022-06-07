64b46e76e37bb8b468829dd6dc912d61c43cf02a
Lojain Mostafa <lojain17mostafa@gmail.com>
2022-06-07 21:24:32 +0200

    qc progress

--- reservation_survey.php
+++ reservation_survey.php
@@ -1,3 +1,16 @@
+<?php
+
+require "models/QCQuestion.php";
+require "models/QCQuestionOption.php";
+require "models/QCQuestionType.php";
+
+$questions = QCQuestion::all();
+
+$questions = array_map(function($value){
+    return array($value, QCQuestionType::find())
+},$questions);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +22,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    <link rel="icon" href="images/logo.png" type="image/png">
+    <link rel="icon" href="imagees/logo.png" type="image/png">
     <title>Feedback</title>
 </head>
 
