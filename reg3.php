996f89fc8bd0ac9b491d9e7e84006fb34c15600b
sarah801 <sarah2017578@miuegypt.edu.eg>
2022-06-07 19:27:33 +0200

    reg3

--- reg3.php
+++ reg3.php
@@ -26,6 +26,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="layout/css/app.css">
+    <link href="style.css" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
@@ -40,51 +41,51 @@
         </a>
     </header>
     <form action="">
-            <div class=" conatainer ">
-                <label for="rooms" class = " text-4xl  font-weight-bold"> reservation options </label>
+            <div class= "conatainer">
+                <label for="rooms" class = "text-4xl font-weight-bold"> reservation options </label>
                 
                 <select required id="rooms" name="rooms">
                     <option disabled selected>Select room type</option>
                     <?php
                     foreach($types as $type){
                         ?>
-                        <option value="<?=$type["id"]; ?>"><?=$type["name"];?></option>
+                        <option value="<?=$type["id"]; ?>"><?=$type["reservation_id"];?></option>
                         <?php
                     }
                     ?>
                     
                 </select>
             </div>
-            <div class="flex p-2" >
-                <div class="bg-white text-center font-bold">
-                    <img src="single(1).jpg" alt="Single" width="200" height="100" />
+              <div class="flex p-2" > 
+                <div class="card1">
+                    <img src="single(1).jpg" alt="Single" />
                     Single
                 </div>
-                <div class="bg-white text-center font-bold">
+                <div class="card3">
                     <img src="teriple.jpg" alt="teriple" width="200" height="100"/>
                     Triple
                 </div>
             </div>
-            <div class="flex p-2">
-             <div class="bg-white text-center font-bold">
+            
+             <div class="card2">
                 <img src="double-double-room.jpg"alt="Double" width="200" height="100" name = "Double" />
                 Double 
              </div>
              
-                         <div class="bg-white text-center font-bold">
+            <div class="card4">
             <img src="queen.jpg" alt="queen" width="200" height="100"/>
             Queen 
             </div>
                     
-                         <div class="bg-white text-center font-bold">
+                         <div class="card5">
               <img src="cabana.jpg" alt="cabana" width="200" height="100"/>
               cabana</div>
 
-                         <div class="bg-white text-center font-bold">
+                         <div class="card6">
                 <img src="suite2.jpg" alt="suite" width="200" height="100"/>
                 Suite 
                          </div>
-                </div>
+        
 
             </div>
             <div>
