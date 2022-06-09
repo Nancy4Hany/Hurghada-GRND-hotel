59ffc67ae014c4f5a864d49c3b0d6d26ceb9f90e
70dakhaled <mahmoud2008975@miuegypt.edu.eg>
2022-06-09 18:36:32 +0200

    add resrvation controller (untested to be reviewed later)

--- controllers/ReservationsController.php
+++ controllers/ReservationsController.php
@@ -24,9 +24,38 @@ class ReservationsController
         $user_reservations = Reservation::find($_GET["id"]);
         return($user_reservations);
     }
+public function add_reservation(){
+        if (isset($_POST["submit"])) {
+
+            // template to enter in reservation database after choosing steps 
+           
+
+            $user_id = $_SESSION["id"];
+            $start_date = date('Y-m-d', strtotime($_POST['birth_date']));
+            $end_date = date('Y-m-d', strtotime($_POST['birth_date']));
+            $name=$_SESSION["name"];
+    
+            $reservation = new Reservation ();
+
+
+            $reservation->data["user_id"] = $user_id; 
+            $reservation->data["start_date"]=$start_date;
+            $reservation->data["end_date"] = $end_date;
+            $reservation->data["status"]  =3;        //status=3 means pending
+            $reservation->data["username"] =$name;                   
+
+
+                if ($reservation->save()) {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+
 
  
-}
+
 
 
 ?>
