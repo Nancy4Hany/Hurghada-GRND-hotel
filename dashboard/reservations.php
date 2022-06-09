67836c97308b190aa8c1893e254c7f2bf7d4bc93
nancy4hany <nancyhany8@gmail.com>
2022-06-09 22:12:15 +0200

    modification in reservations

--- dashboard/reservations.php
+++ dashboard/reservations.php
@@ -89,14 +89,16 @@ include "../includes/dashboard/header.php";
                         </svg>
                       </button></a>
 
-                    <a href="delete.php?type=reservation.php&id=<?= $reservation["id"]; ?>">
-                      <button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete">
+
+                    
+                    <!-- <a href="delete.php?type=reservation.php&id=<?= $reservation["id"]; ?>"> -->
+                      <button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete" onclick ="if(confirm ('Are you sure you want to delete this reservation?')){window.location.href='../controllers/delete-reservation.php?id=<?= $reservation["id"]; ?>'}">
                         <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                           <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
 
                         </svg>
                       </button>
-                    </a>
+                    <!-- </a> -->
                     <a href="view-reservation.php?id=<?= $reservation["id"]; ?>">
                       <button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray">
                         <i class="las la-eye text-xl"></i>
